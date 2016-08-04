db6cbf963b39709469df6488d4460e51e2456471
Daniel Gehrig <daniel.gehrig1@gmx.ch>
2016-08-04 15:13:39 +0200

    Initial Commit

---
+++ VacuumSimulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VacuumSimulator
+//
+//  Created by Daniel Gehrig on 04/08/16.
+//  Copyright © 2016 Daniel Gehrig. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

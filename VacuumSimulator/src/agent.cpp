d7faa0a71ba5d56856925b396fb4311724a204cb
Daniel Gehrig <danielgehrig@daniels-macbook-pro-3.home>
2016-09-22 16:44:11 +0200

    continue on blind localised

--- VacuumSimulator/src/agent.cpp
+++ VacuumSimulator/src/agent.cpp
@@ -83,6 +83,8 @@ void BlindLocalize()
     localize_tree.Search();
     vector<char> action_sequence = localize_tree.GetSolution();
     
+    strategy_.AddActionSequence(action_sequence);
+    array<int, 2> location_ = localize_tree.GetStart();
     
-    
+    localize_tree.Clear();
 }
